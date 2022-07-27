532947f27288d774f135419acab82d5a48a51bc6
Ramya Goud <vemularamya85@gmail.com>
2022-07-27 10:01:38 +0530

    first commit

---
+++ src/main/java/com/example/ComprenhensivePractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprenhensivePractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprenhensivePractiseApplication.class, args);
+	}
+
+}
